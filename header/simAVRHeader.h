a2fbab90a63d24771ea22733d989bcf790ab9cd0
mchoi041 <38056099+mchoi041@users.noreply.github.com>
2022-05-25 16:47:06 -0700

    Add files via upload

--- header/simAVRHeader.h
+++ header/simAVRHeader.h
@@ -7,21 +7,21 @@
 #include <avr/sleep.h>
 #include "include/simavr/avr/avr_mcu_section.h"
 AVR_MCU(F_CPU,"atmega1284");
-AVR_MCU_VCD_FILE("build/results/Custom_Lab_Project_trace.vcd",1000);
+AVR_MCU_VCD_FILE("build/results/Lab3_bitManipulation_trace.vcd",1000);
 
 const struct avr_mmcu_vcd_trace_t _mytrace[] _MMCU_ = {
-    { AVR_MCU_VCD_SYMBOL("PINA0"), .mask = 1 << 0,.what = (void*)&PINA, } , // Example individual pin
-    { AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*)&PORTB, } , // Example full port
+	{ AVR_MCU_VCD_SYMBOL("PINA0"), .mask = 1 << 0,.what = (void*)&PINA, } , // Example individual pin
+	{ AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*)&PORTB, } , // Example full port
 };
 
 /* Function to output through UART */
 static int uart_putchar(char c, FILE *stream) {
-    if (c == '\n') {
-        uart_putchar('\r',stream);
-    }
-    loop_until_bit_is_set(UCSR0A,UDRE0);
-    UDR0 = c;
-    return 0;
+	if (c == '\n') {
+		uart_putchar('\r',stream);
+	}
+	loop_until_bit_is_set(UCSR0A,UDRE0);
+	UDR0 = c;
+	return 0;
 }
 
 /* Setup filestream for debugging */
